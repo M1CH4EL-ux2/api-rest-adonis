6aa73960a534be49daee373fb37d369bd568072e
M1CH4EL-ux2 <joaomichael912@gmail.com>
2022-05-22 11:54:41 -0300

    Ajuste no metodo show do Controller moments

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -45,6 +45,8 @@ export default class MomentsController {
   public async show({ params }: HttpContextContract) {
     const moment = await Moment.findOrFail(params.id)
 
+    await moment.load('comments')
+
     return {
       data: moment,
     }
